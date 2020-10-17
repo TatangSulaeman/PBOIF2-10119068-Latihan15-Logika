12066c6b86b13db91db5e58290abd27f065c6272
amtat <amtat@DESKTOP-3DL3458>
2020-10-17 17:52:47 +0700

    PBOIF2-10119068-Latihan15-Logika

---
+++ src/pboif2/pkg10119068/latihan15/logika/PBOIF210119068Latihan15Logika.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119068.latihan15.logika;
+
+/**
+ *
+ * @author amtat
+ * NAMA     : TATANG SULAEMAN
+ * KELAS    : PBOIF2
+ * NIM      : 10119068
+ * Deskripsi program    : Logika
+ */
+public class PBOIF210119068Latihan15Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        boolean a = true;
+        boolean b = false;
+        System.out.println("a && = " + (a && b));
+        System.out.println("a || = " + (a || b));
+        System.out.println("!a = " + !a);
+        System.out.println("!(a && b) = " +!(a && b));
+        
+    }
+    
+}
